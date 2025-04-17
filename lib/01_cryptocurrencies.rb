f079737694ce4fe34ed265a2c00413b575f293f5
christart-code <christleckossa@gmail.com>
2025-04-17 21:59:15 +0100

    Readme

---
+++ lib/01_cryptocurrencies.rb
@@ -0,0 +1,97 @@
+# Liste des noms de cryptomonnaies
+cryptos = ["Bitcoin", "Ethereum", "XRP", "Bitcoin Cash", "EOS", "Litecoin", "Cardano", "Stellar", 
+"IOTA", "Tether", "NEO", "TRON", "Monero", "Dash", "Ethereum Classic", "NEM", "Binance Coin", 
+"VeChain", "OmiseGO", "Qtum", "Zcash", "Ontology", "ICON", "Bytecoin", "Lisk", "Zilliqa", 
+"Bitcoin Gold", "Aeternity", "Decred", "0x", "Steem", "BitShares", "Bytom", "Siacoin", "Augur", 
+"Verge", "Nano", "Maker", "Bitcoin Diamond", "Dogecoin", "DigiByte", "Waves", "Populous", "Golem", 
+"RChain", "Status", "Stratis", "Wanchain", "Basic Attention Token", "IOST", "Mixin", "Hshare", 
+"Bitcoin Private", "Nebulas", "KuCoin Shares", "Waltonchain", "Loopring", "DigixDAO", "aelf", 
+"Huobi Token", "Aion", "GXChain", "Mithril", "Komodo", "Ardor", "Ark", "Bancor", "MaidSafeCoin", 
+"MonaCoin", "Syscoin", "FunFair", "ReddCoin", "CyberMiles", "Kyber Network", "Dentacoin", "Monaco", 
+"Emercoin", "Ethos", "Gas", "Elastos", "Cryptonex", "PIVX", "Loom Network", "Dropil", "Enigma",
+ "Veritaseum", "Decentraland", "Kin", "WAX", "Theta Token", "Cortex", "Power Ledger", "MOAC", 
+ "Centrality", "WaykiChain", "Nxt", "Polymath", "Electroneum", "QASH", "Bibox Token", "Nuls", 
+ "SmartCash", "Factom", "TrueUSD", "Paypex", "Gifto", "Scry.info", "Game.com", "Substratum", 
+ "Storm", "ZCoin", "Byteball Bytes", "ChainLink", "Fusion", "TenX", "Blocknet", "ZenCash", 
+ "Docademic", "SALT", "Nexus", "Matrix AI Network", "Storj", "BitcoinDark", "TokenPay", "Revain", 
+ "Dragonchain", "MediShares", "Civic", "Iconomi", "Achain", "High Performance Blockchain", 
+ "SingularityNET", "Bluzelle", "Time New Bank", "iExec RLC", "Nucleus Vision", "Nexty", "Selfkey", 
+ "Skycoin", "Nexo", "DeepBrain Chain", "BnkToTheFuture", "Holo", "SONM", "CyberVein", "Neblio", 
+ "Request Network", "Ruff", "Aragon", "SmartMesh", "DEW", "Santiment Network Token", "Arcblock", 
+ "Streamr DATAcoin", "True Chain", "Bottos", "Dai", "Gnosis", "THEKEY", "Cindicator", "All Sports", 
+ "Po.et", "Quantstamp", "Asch", "eosDAC", "Storiqa", "BLOCKv", "ODEM", "EDUCare", "Ambrosus", 
+ "Enjin Coin", "Groestlcoin", "POA Network", "Particl", "Ignis", "Credits", "GameCredits", "SophiaTX", 
+ "Vertcoin", "Bread", "DigitalNote", "Dynamic Trading Rights", "Raiden Network Token", "Genaro Network", 
+ "BOScoin", "CRYPTO20", "Ubiq", "SIRIN LABS Token", "Cube", "Odyssey", "Dent", "Peercoin", "Aston", 
+ "Safe Exchange Token", "ETHLend", "Electra", "Crypterium", "Pillar", "Endor Protocol", "ZClassic", 
+ "NANJCOIN", "NKN", "IoT Chain", "IHT Real Estate Protocol", "Metal", "DATA", "Bitcoin Interest", 
+ "Telcoin", "Trade Token", "Tokenomy", "SingularDTV", "Genesis Vision", "IoTeX", "Litecoin Cash", 
+ "Bitcore", "Edgeless", "Burst", "Unobtanium", "Ternio", "BridgeCoin", "Rock", "Bezant", "AdEx", 
+ "NavCoin", "Red Pulse", "Ink", "Internet Node Token", "BitBay", "OriginTrail", 
+ "Distributed Credit Chain", "Primecoin", "Eidoo", "bitCNY", "Jibrel Network", 
+ "Einsteinium", "Republic Protocol", "Apex", "Stakenet", "Lympo", "Gulden", "TokenCard", 
+ "WhiteCoin", "SpankChain", "SwftCoin", "Delphy", "Eximchain", "Viacoin", "LGO Exchange", 
+ "Quantum Resistant Ledger", "Ripio Credit Network", "OST", "Linda", "district0x", "Wagerr", 
+ "SaluS", "FairCoin", "Universa", "PayPie", "YEE", "BABB", "APIS", "FuzeX", "DAEX", "Banca", 
+ "TokenClub", "CloakCoin", "TomoChain", "PRIZM", "GINcoin", "LinkEye", "QuarkChain", "Presearch", "Zebi", "Counterparty", "MinexCoin", "AirSwap", "BitRent", "Metaverse ETP", "Aeon", "Namecoin", "Ivy", "0chain", "AppCoins", "UTRUST", "DATx", "KickCoin", "WePower", "Cryptaur", "Wings", "Tierion", "CPChain", "Swarm", "INS Ecosystem", "Steem Dollars", "ION", "Havven", "Modum", "Ravencoin", "Sakura Bloom", "Feathercoin", "Hydro Protocol", "MobileGo", "Spectre.ai Decentralized Prediction Market", "DECENT", "LBRY Credits", "PACcoin", "Etherparty", "LoyalCoin", "WaBi", "ugChain", "Lunyr", "Phore", "Melon", "Zeepin", "ATN", "CasinoCoin", "NAGA", "Blox", "YOYOW", "OracleChain", "TaaS", "MARK.SPACE", "Peerplays", "CashBet Coin", "DecentBet", "XTRABYTES", "Credo", "QLINK", "Pura", "Mothership", "SelfSell", "MediBloc [ERC20]", "Datum", "Electrify.Asia", "HTMLCOIN", "Merculet", "Experience Points", "Primas", "XinFin Network", "Mobius", "TrustNote", "VIBE", "Decision Token", "Qbao", "SHIELD", "Hive Project", "Humaniq", "Aurora DAO", "MediBloc [QRC20]", "Karma", "OneRoot Network", "Cashaa", "Constellation", "ECC", "Dock", "Oyster", "QunQun", "Bankex", "Envion", "U Network", "XMax", "FirstBlood", "Mercury", "Sentinel", "COSS", "Zeusshield", "Pepe Cash", "LEOcoin", "Crowd Machine", "Hydrogen", "Fortuna", "Alphacat", "Soarcoin", "Shift", "Cofound.it", "DNotes", "CEEK VR", "TE-FOOD", "Blackmoon", "AICHAIN", "ProChain", "Viberate", "ColossusXT", "Elastic", "Synereo", "BlockMason Credit Protocol", "Datawallet", "Trinity Network Credit", "Cobinhood", "Covesting", "bitUSD", "Everex", "HempCoin", "SpaceChain", "Bitcoin Green", "DIMCOIN", "Triggers", "iXledger", "Maecenas", "Bismuth", "Propy", "Ormeus Coin", "Medicalchain", "PotCoin", "Unikoin Gold", "STK", "Measurable Data Token", "Quantum", "LIFE", "GridCoin", "StrongHands", "Pascal Coin", "Rialto", "Boolberry", "Bloom", "Olympus Labs", "Proxeus", "Bean Cash", "Lamden", "Oxycoin", "Neurotoken"]
+
+# Liste des prix correspondants (chaînes de caractères)
+prices = ["6558.07", "468.95", "0.487526", "762.84", "8.86", "85.26", "0.151268", "0.206756", "1.18", "0.998033", "39.47", "0.038865", "140.57", "241.10", "16.40", "0.184721", "14.05", "2.64", "7.96", "8.98", "180.36", "5.09", "1.68", "0.003487", "5.63", "0.079253", "29.05", "2.10", "68.39", "0.908975", "1.77", "0.160134", "0.397237", "0.010946", "35.03", "0.025425", "2.61", "551.89", "1.97", "0.002620", "0.028549", "2.99", "8.01", "0.348054", "0.804896", "0.080015", "2.75", "2.50", "0.248590", "0.027269", "517.26", "5.25", "10.96", "4.85", "2.40", "6.70", "0.358418", "102.17", "0.746954", "3.73", "1.08", "2.95", "0.509409", "1.62", "0.160924", "1.55", "3.04", "0.325683", "2.41", "0.263632", "0.028072", "0.004779", "0.193872", "0.980281", "0.000404", "8.21", "3.07", "1.65", "12.59", "23.79", "2.70", "2.10", "0.195202", "0.005923", "1.55", "55.92", "0.107892", "0.000149", "0.133220", "0.162237", "0.716056", "0.282753", "2.88", "0.129984", "0.609465", "0.098241", "0.351648", "0.013166", "0.269008", "0.877180", "2.29", "0.085871", "10.32", "1.00", "1.28", "0.164247", "0.257568", "0.114499", "0.227401", "0.019288", "16.59", "129.95", "0.235255", "2.72", "0.692377", "14.43", "17.74", "0.251847", "1.22", "1.25", "0.480287", "0.522453", "53.76", "4.35", "0.359320", "0.280473", "0.064773", "0.191627", "0.660997", "0.117242", "1.94", "0.117656", "0.346296", "0.026830", "0.772085", "0.014067", "0.000588", "0.029424", "6.02", "0.107378", "0.039282", "0.092631", "0.000434", "0.158162", "0.055067", "4.16", "0.078483", "0.059246", "1.92", "0.038230", "0.518476", "0.850364", "0.567516", "0.077973", "1.13", "0.102616", "0.999544", "46.19", "0.010800", "0.034929", "0.059021", "0.021221", "0.078829", "0.512577", "0.071594", "0.005791", "0.023575", "0.260800", "0.102411", "0.318643", "0.060716", "0.652179", "0.209810", "4.56", "0.058817", "0.321585", "0.688413", "0.229078", "0.966402", "0.484498", "0.006183", "0.034570", "0.815259", "0.167460", "0.091204", "1.02", "0.965587", "0.173413", "0.006260", "0.008162", "0.003608", "1.53", "0.045558", "0.023820", "0.032964", "0.001381", "0.452294", "0.136147", "0.066920", "8.14", "0.001814", "0.232160", "0.576665", "0.067643", "1.36", "0.007336", "1.83", "0.001089", "0.369885", "0.227105", "0.055147", "8.86", "0.028273", "0.055691", "2.06", "0.330947", "0.015921", "156.64", "0.089900", "1.14", "0.078437", "0.090307", "0.416111", "0.483585", "0.037275", "0.064879", "0.101208", "0.029402", "0.110673", "0.033825", "1.21", "0.986037", "0.162258", "0.177991", "0.130821", "0.050088", "0.065677", "0.405910", "0.038079", "0.067437", "1.11", "0.107803", "0.089120", "0.007312", "0.620850", "0.388518", "1.13", "0.216408", "0.492551", "0.051678", "0.073124", "0.002734", "0.040696", "0.133191", "24.11", "0.457143", "0.013161", "0.293034", "0.017687", "0.001144", "0.005323", "0.032126", "0.063085", "0.001304", "0.048455", "4.51", "0.421119", "1.05", "10.63", "0.036700", "0.061340", "0.140479", "0.046065", "8.27", "5.50", "0.143001", "0.025453", "0.592763", "1.34", "1.43", "0.067758", "0.525159", "0.209338", "0.065902", "0.014659", "0.041221", "0.045232", "0.002958", "0.222999", "0.046457", "0.068920", "0.387482", "0.648186", "1.27", "0.906731", "0.311022", "1.05", "0.015602", "0.007419", "0.091636", "0.026733", "0.189460", "0.225985", "0.357193", "0.109541", "0.004485", "0.037284", "0.003645", "0.397335", "0.031545", "7.78", "1.30", "29.70", "0.035598", "0.211130", "0.000494", "0.307497"];  
+
+# 1. Création du hash associant chaque crypto à son prix (conversion en Float)
+
+# my_hash = cryptos.zip(prices.lazy.map(&:to_f)).to_h
+
+# 2 & 3. Recherche des cryptos avec les valeurs les plus élevées et les plus basses
+my_hash = {
+  "Bitcoin" => 30000,
+  "Ethereum" => 1800,
+  "Dogecoin" => 0.07,
+  "Ripple" => 0.5,
+  "Litecoin" => 30000 # exemple égal à Bitcoin
+}
+
+max_price = -Float::INFINITY
+min_price = Float::INFINITY
+
+highest_cryptos = []
+lowest_cryptos = []
+
+my_hash.each do |name, price|
+  if price > max_price
+    max_price = price
+    highest_cryptos = [name]
+  elsif price == max_price
+    highest_cryptos << name
+  end
+
+  if price < min_price
+    min_price = price
+    lowest_cryptos = [name]
+  elsif price == min_price
+    lowest_cryptos << name
+  end
+end
+
+puts "Crypto(s) avec le prix le plus élevé (#{max_price}) : #{highest_cryptos.join(', ')}"
+puts "Crypto(s) avec le prix le plus bas (#{min_price}) : #{lowest_cryptos.join(', ')}"
+
+# 4. Comptage des cryptos contenant 'coin' (insensible à la casse)
+coin_count = my_hash.keys.count { |name| name.downcase.include?('coin') }
+puts "Nombre de cryptos contenant 'coin' : #{coin_count}"
+
+
+# 5. Sélection des cryptos dont le prix est inférieur à 6000
+under_6000 = my_hash.select { |_name, price| price < 6000 }
+under_6000_names = under_6000.keys  # noms correspondants
+
+# 6. Recherche de la crypto la plus chère parmi celles à moins de 6000
+top_under_6000_price = under_6000.values.max
+top_under_6000 = under_6000.select { |_name, price| price == top_under_6000_price }.keys
+
+# 7. Affichage des résultats
+puts "Cryptos de valeur maximale : #{highest_cryptos.join(', ')} (#{max_price})"
+puts "Cryptos de valeur minimale : #{lowest_cryptos.join(', ')} (#{min_price})"
+puts "Nombre de cryptos contenant 'coin' : #{coin_count}"
+puts "Cryptos dont le prix < 6000 : #{under_6000_names.join(', ')}"
+puts "Crypto la plus chère parmi celles < 6000 : #{top_under_6000.join(', ')} (#{top_under_6000_price})"
